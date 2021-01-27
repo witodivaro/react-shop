69e4a8406a2b4ec9304f901197a0a5f750e648c1
Vitaly Litvinovich <vitaly@wsa-242-73b.local>
2021-01-27 11:45:49 +0300

    Directory is a component, it's styling is done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 
 const App = () => {
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const Homepage = () => {
   return (
     <div className="homepage">
-      <div className="directory-menu"></div>
+      <Directory />
     </div>
   );
 };
