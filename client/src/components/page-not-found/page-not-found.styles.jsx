6d0ccf7a782077a04ea7b5a260f24a434cf8cab1
Vitaly Litvinovich <vitaly@wsa-242-73b.local>
2021-02-09 19:53:22 +0300

    styles: remove page-not-found styling horizontal-scroll bug

--- client/src/components/page-not-found/page-not-found.styles.jsx
+++ client/src/components/page-not-found/page-not-found.styles.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const PageNotFoundContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100%;
   height: 60vw;
   display: flex;
